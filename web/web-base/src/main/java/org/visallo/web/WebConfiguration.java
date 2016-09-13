5e207a631e7406d13fe463ad7ce9bf6ffb3d4915
Jason Harwig <jason.harwig@gmail.com>
2016-09-13 13:47:31 -0500

    Fix default open street map url for ol3

--- web/web-base/src/main/java/org/visallo/web/WebConfiguration.java
+++ web/web-base/src/main/java/org/visallo/web/WebConfiguration.java
@@ -114,7 +114,7 @@ public class WebConfiguration {
                 PROPERTY_METADATA_CONFIDENCE.getDataType()));
 
         DEFAULTS.put(MAP_PROVIDER, MapProvider.OSM.toString());
-        DEFAULTS.put(MAP_PROVIDER_OSM_URL, "https://{a-c}.tile.openstreetmap.org/${z}/${x}/${y}.png");
+        DEFAULTS.put(MAP_PROVIDER_OSM_URL, "https://{a-c}.tile.openstreetmap.org/{z}/{x}/{y}.png");
     }
 
     public static class PropertyMetadata {
